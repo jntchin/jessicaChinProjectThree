4ceb505566e446c99abb973c503982438d387e5b
Jessica Chin <jntchin@gmail.com>
2019-08-20 10:45:26 -0400

    more pseudocode

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
-//pseudocode goes here
 
 //on a click event, animate a 20-sided polygon (rotate) 
 //add message (html) saying "click to stop"
 //on another click event, stop the animation
 //add message saying "click for a new roll"
 //show a random number from 1 to 20 on the die itself
-//show a total value
+//store that number
+//show that number as the total
 
 //listen for a form submit from the drop-down menu
 //on a submit, add that number to the die value
@@ -14,5 +14,5 @@
 
 
 //STRETCH GOALS
-//on a click event, animate other polygons (6, 4, 10, 12, 8-sided dice)
-//
+//add another drop-down menu to select the type of roll (different ability checks, attack rolls, etc.). The values in the menu would be displayed when the total score is displayed, e.g. "perception check: 14"
+//animate other polygons based on click events (6, 4, 10, 12, 8-sided dice)
