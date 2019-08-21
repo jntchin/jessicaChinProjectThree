a9f375e49a56077a1f4ba615b9b999e1a6b8c6d8
Jessica Chin <jntchin@gmail.com>
2019-08-21 17:42:30 -0400

    5:42 p.m. Wednesday commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,25 @@
-
+$(document).ready(function(){
 //on a click event, animate a 20-sided polygon (rotate) 
 //add message (html) saying "click to stop"
+    $('body').on('click', function(){
+        console.log(`Click again to stop`);
+    }); 
+
 //on another click event, stop the animation
 //add message saying "click for a new roll"
 //show a random number from 1 to 20 on the die itself
 //store that number
 //show that number as the total
 
+
+    $('form').on('submit', function(event){
+        event.preventDefault();
+
 //listen for a form submit from the drop-down menu
 //on a submit, add that number to the die value
 //prevent default / refresh for form submission
 //show a total score
-
-
-$(document).ready(function(){
-
-    $('body').on('click', function(){
-        console.log(`Click again to stop`);
-    }); 
-
-    // $('body').on('click', function(){
-    //     console.log(`You already did this`);
-    // })
-
+    
 });
 
 
@@ -31,4 +28,4 @@ $(document).ready(function(){
 //add another drop-down menu to select the type of roll (different ability checks, attack rolls, etc.). The values in the menu would be displayed when the total score is displayed, e.g. "perception check: 14"
 //animate other polygons based on click events (6, 4, 10, 12, 8-sided dice)
 
-
+});
