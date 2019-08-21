ebe6e5e42d43eb3d250092646226e3c69820434c
Jessica Chin <jntchin@gmail.com>
2019-08-21 10:05:26 -0400

    10:05 a.m. Wednesday

--- scripts/script.js
+++ scripts/script.js
@@ -13,6 +13,18 @@
 //show a total score
 
 
+$(document).ready(function(){
+
+    $(body).on('click', function(){
+        console.log(`Click again to stop`);
+    });
+
+});
+
+
+
 //STRETCH GOALS
 //add another drop-down menu to select the type of roll (different ability checks, attack rolls, etc.). The values in the menu would be displayed when the total score is displayed, e.g. "perception check: 14"
 //animate other polygons based on click events (6, 4, 10, 12, 8-sided dice)
+
+
