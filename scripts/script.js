86be0d5bd1564296c4120689ef73a7b21463a559
Jessica Chin <jntchin@gmail.com>
2019-08-23 10:42:37 -0400

    10:42 a.m. Friday commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,17 @@
 // flag to indicate when the page first laads, the die is not moving.
-let isDieMoving = false;
+let dieMoving = false;
 
 $(document).ready(function(){
 
     $('p.clickPrompt').append(`Click here to roll the die`);
+    // $('.rotating').empty().css();
 
     $('.d20').on('click', function(){   
         //initially says "Click here to roll the die"
-        if (isDieMoving === false){
+        if (dieMoving === false){
             $('p.clickPrompt').empty().html(`Click to stop`);
+            // $('section.d20').append('.rotating');
+
         }
         else {
             $('p.clickPrompt').empty().html(`Click here to roll the die`);
@@ -17,7 +20,7 @@ $(document).ready(function(){
         };
 
         // console.log(`inside the die statement`)};
-        isDieMoving = !isDieMoving; 
+        dieMoving = !dieMoving; 
 
             $('form').on('submit', function(event){
                 event.preventDefault();
