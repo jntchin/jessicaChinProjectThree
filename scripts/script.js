386a0f91c2688c37e0fa98be064746089927ce88
Jessica Chin <jntchin@gmail.com>
2019-08-23 12:54:16 -0400

    12:54 p.m. Friday commit organized code

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,45 @@
-// flag to indicate when the page first laads, the die is not moving.
-let dieMoving = false;
+const dieRollApp = {};
 
-$(document).ready(function(){
-
+dieRollApp.dieMoving = false;
+dieRollApp.initialState = function(){ 
     $('p.clickPrompt').append(`Click here to roll the die`);
     $('.d20').css('animation-play-state', 'paused');
+};
+
+dieRollApp.rollDie = function(){
+    $('.d20').on('click touch', function(){   
+    //initially says "Click here to roll the die"
+    if (dieRollApp.dieMoving === false){
+        $('p.clickPrompt').empty().html(`Click to stop`);
+        $('.d20').css('animation-play-state', 'running');
+    }
+    else {
+        $('p.clickPrompt').empty().html(`Click here to roll the die`);
+        dieRollApp.dieRoll = Math.floor(Math.random()*(0-20))+21;
+        $('.emptyValue').empty().html(dieRollApp.dieRoll);
+        $('.d20').css('animation-play-state', 'paused');
+    };
+
+    // console.log(`inside the die statement`)};
+    dieRollApp.dieMoving = !dieRollApp.dieMoving; 
+
+    })
+};
+
+
+
+// flag to indicate when the page first laads, the die is not moving.
+
+
+$(document).ready(function(){
+    dieRollApp.initialState();
+    dieRollApp.rollDie();
+    
+  
 
     // $('.rotating').empty().css();
 
-    $('.d20').on('click touch', function(){   
-        //initially says "Click here to roll the die"
-        if (dieMoving === false){
-            $('p.clickPrompt').empty().html(`Click to stop`);
-            $('.d20').css('animation-play-state', 'running');
-        }
-        else {
-            $('p.clickPrompt').empty().html(`Click here to roll the die`);
-            let dieRoll = Math.floor(Math.random()*(0-20))+21;
-            $('.emptyValue').empty().html(dieRoll);
-            $('.d20').css('animation-play-state', 'paused');
-        };
-
-        // console.log(`inside the die statement`)};
-        dieMoving = !dieMoving; 
-    
-    })
+   
 
     $('form').on('submit', function(event){
         event.preventDefault();
@@ -33,8 +48,11 @@ $(document).ready(function(){
     $('button').on('click', function(){
         // $('p.clickPrompt').empty().html(`Click here to roll the die`);
         // let dieRoll = Math.floor(Math.random()*(0-20))+21;
-        let finalDieRoll = dieRoll + 4; 
-        console.log(finalDieRoll);
+        // let modifier = document.getElementById('number');
+        // console.log(modifier);
+        
+        dieRollApp.finalDieRoll = dieRollApp.dieRoll + 4; 
+        console.log(dieRollApp.dieRoll,dieRollApp.finalDieRoll);
     });
 
    
