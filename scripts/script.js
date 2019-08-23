29d3b373fda21b0b8e15052a6a6092345af99652
Jessica Chin <jntchin@gmail.com>
2019-08-23 12:08:15 -0400

    12:07 p.m. Friday commit - finding scope issues with button click

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 
     // $('.rotating').empty().css();
 
-    $('.d20').on('click', function(){   
+    $('.d20').on('click touch', function(){   
         //initially says "Click here to roll the die"
         if (dieMoving === false){
             $('p.clickPrompt').empty().html(`Click to stop`);
@@ -23,9 +23,20 @@ $(document).ready(function(){
 
         // console.log(`inside the die statement`)};
         dieMoving = !dieMoving; 
-
-            $('form').on('submit', function(event){
-                event.preventDefault();
-            });
+    
     })
+
+    $('form').on('submit', function(event){
+        event.preventDefault();
+    });
+
+    $('button').on('click', function(){
+        // $('p.clickPrompt').empty().html(`Click here to roll the die`);
+        // let dieRoll = Math.floor(Math.random()*(0-20))+21;
+        let finalDieRoll = dieRoll + 4; 
+        console.log(finalDieRoll);
+    });
+
+   
+
 });
