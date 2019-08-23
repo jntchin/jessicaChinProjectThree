556e61c12c840c3551ff776bb707eb605b365a4c
Jessica Chin <jntchin@gmail.com>
2019-08-23 10:58:42 -0400

    10:58 a.m. Friday commit- animation pause and run added

--- scripts/script.js
+++ scripts/script.js
@@ -4,19 +4,21 @@ let dieMoving = false;
 $(document).ready(function(){
 
     $('p.clickPrompt').append(`Click here to roll the die`);
+    $('.d20').css('animation-play-state', 'paused');
+
     // $('.rotating').empty().css();
 
     $('.d20').on('click', function(){   
         //initially says "Click here to roll the die"
         if (dieMoving === false){
             $('p.clickPrompt').empty().html(`Click to stop`);
-            // $('section.d20').append('.rotating');
-
+            $('.d20').css('animation-play-state', 'running');
         }
         else {
             $('p.clickPrompt').empty().html(`Click here to roll the die`);
             let dieRoll = Math.floor(Math.random()*(0-20))+21;
             $('.emptyValue').empty().html(dieRoll);
+            $('.d20').css('animation-play-state', 'paused');
         };
 
         // console.log(`inside the die statement`)};
